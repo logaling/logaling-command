474c8db12e840a966b6173f13deb56ec00ad5d4a
SUZUKI Miho <adzuki34@gmail.com>
2011-10-24 17:50:31 +0900

    [finish #19831685] enabled to specify the path

--- lib/logaling-command.rb
+++ lib/logaling-command.rb
@@ -6,8 +6,7 @@ require "fileutils"
 
 module Logaling
   def exec(action, options)
-    fname = [options[:glossary], options[:from], options[:to]].join(".")
-    options[:path] = File.join(LOGALING_HOME, "#{fname}.yml")
+    options[:path] = glossary_path(options)
 
     case action
     when "create"
@@ -22,7 +21,8 @@ module Logaling
     if File.exists?(options[:path])
       puts "glossary #{options[:glossary]} is already exists"
     else
-      Dir::mkdir(LOGALING_HOME) unless FileTest::directory?(LOGALING_HOME)
+      dirname = File::dirname(options[:path])
+      Dir::mkdir(dirname) unless FileTest::directory?(dirname)
       FileUtils.touch(options[:path])
     end
   end
@@ -44,5 +44,15 @@ module Logaling
     return true
   end
 
-  module_function :exec, :create, :check_options
+  def glossary_path(options)
+    dir, file = File::split(options[:glossary])
+    if dir == "."
+      fname = [options[:glossary], options[:from], options[:to]].join(".")
+      return File.join(LOGALING_HOME, "#{fname}.yml")
+    else
+      return options[:glossary]
+    end
+  end
+
+  module_function :exec, :create, :check_options, :glossary_path
 end
