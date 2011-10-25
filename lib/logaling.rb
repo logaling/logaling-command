87971e07f1eb9b8f8c9cb095318dd4d5fa4c3f25
SHIMADA Koji <koji.shimada@enishi-tech.com>
2011-10-25 10:50:15 +0900

    move Logaling::Command to lib/logaling/command.rb

--- lib/logaling.rb
+++ lib/logaling.rb
@@ -1,34 +1,6 @@
 # -*- encoding: utf-8 -*-
 
-require 'psych'
-require "yaml"
-require "fileutils"
-
 require "logaling/version"
-require "logaling/glossary"
+require "logaling/command"
 
-module Logaling
-  module Command
-    def exec(action, options)
-      glossary = Glossary.new(options[:glossary], options[:from], options[:to])
-
-      case action
-      when "create"
-        glossary.create
-      when "add"
-        glossary.add(options[:keyword], options[:translation], options[:note])
-      when "delete"
-        glossary.delete(options[:keyword], options[:translation])
-      when "lookup"
-        glossary.lookup(options[:keyword])
-      else
-        puts "command '#{action}' not found."
-      end
-    rescue CommandFailed => e
-      STDERR.puts(e.message)
-      exit 1
-    end
-
-    module_function :exec
-  end
-end
+module Logaling; end
---
+++ lib/logaling/command.rb
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+
+require 'psych'
+require "yaml"
+require "fileutils"
+
+require "logaling/glossary"
+
+module Logaling::Command
+  def exec(action, options)
+    glossary = Glossary.new(options[:glossary], options[:from], options[:to])
+
+    case action
+    when "create"
+      glossary.create
+    when "add"
+      glossary.add(options[:keyword], options[:translation], options[:note])
+    when "delete"
+      glossary.delete(options[:keyword], options[:translation])
+    when "lookup"
+      glossary.lookup(options[:keyword])
+    else
+      puts "command '#{action}' not found."
+    end
+  rescue CommandFailed => e
+    STDERR.puts(e.message)
+    exit 1
+  end
+
+  module_function :exec
+end
