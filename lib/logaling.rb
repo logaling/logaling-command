e1bbcc6fa11e69b76e7639a525cd92777f401191
SHIMADA Koji <koji.shimada@enishi-tech.com>
2011-10-26 09:59:54 +0900

    [#20061071]use thor instead of optparse

--- lib/logaling.rb
+++ lib/logaling.rb
@@ -1,6 +1,5 @@
 # -*- encoding: utf-8 -*-
 
-require "logaling/version"
 require "logaling/command"
 
 module Logaling; end
--- lib/logaling/command.rb
+++ lib/logaling/command.rb
@@ -1,29 +1,49 @@
 # -*- encoding: utf-8 -*-
 
+require 'thor'
 require "logaling/glossary"
 
-module Logaling::Command
-  def exec(action, options)
-    glossary = Glossary.new(options[:glossary], options[:from], options[:to])
+class Logaling::Command < Thor
+  VERSION = "0.0.1"
 
-    case action
-    when "create"
-      glossary.create
-    when "add"
-      glossary.add(options[:keyword], options[:translation], options[:note])
-    when "delete"
-      glossary.delete(options[:keyword], options[:translation])
-    when "update"
-      glossary.update(options[:keyword], options[:translation], options[:new_translation], options[:note])
-    when "lookup"
-      glossary.lookup(options[:keyword])
-    else
-      puts "command '#{action}' not found."
-    end
-  rescue CommandFailed => e
-    STDERR.puts(e.message)
-    exit 1
+  map '-c' => :create,
+      '-a' => :add,
+      '-d' => :delete,
+      '-u' => :update,
+      '-l' => :lookup
+
+  desc 'create', 'Create glossary.'
+  method_options glossary: :string, from: :string, to: :string
+  def create
+    glossary = Logaling::Glossary.new(options[:glossary], options[:from], options[:to])
+    glossary.create
   end
 
-  module_function :exec
+  desc 'add', 'Add term to glossary.'
+  method_options glossary: :string, from: :string, to: :string, keyword: :string, translation: :string, note: :string
+  def add
+    glossary = Logaling::Glossary.new(options[:glossary], options[:from], options[:to])
+    glossary.add(options[:keyword], options[:translation], options[:note])
+  end
+
+  desc 'delete', 'Delete term.'
+  method_options glossary: :string, from: :string, to: :string, keyword: :string, translation: :string
+  def delete
+    glossary = Logaling::Glossary.new(options[:glossary], options[:from], options[:to])
+    glossary.delete(options[:keyword], options[:translation])
+  end
+
+  desc 'update', 'Update term.'
+  method_options glossary: :string, from: :string, to: :string, keyword: :string, translation: :string, new_translation: :string
+  def update
+    glossary = Logaling::Glossary.new(options[:glossary], options[:from], options[:to])
+    glossary.update(options[:keyword], options[:translation], options[:new_translation], options[:note])
+  end
+
+  desc 'lookup', 'Lookup terms.'
+  method_options glossary: :string, from: :string, to: :string, keyword: :string
+  def lookup
+    glossary = Logaling::Glossary.new(options[:glossary], options[:from], options[:to])
+    glossary.lookup(options[:keyword])
+  end
 end
