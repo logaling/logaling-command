8929e7156f8502afbe9469de67f9478dc1850c0d
SHIMADA Koji <koji.shimada@enishi-tech.com>
2011-11-10 21:42:09 +0900

    refactor spec for Logaling::Glossary

--- spec/logaling/glossary_spec.rb
+++ spec/logaling/glossary_spec.rb
@@ -4,40 +4,41 @@ require "fileutils"
 
 module Logaling
   describe Glossary do
-    before do
-      @path = File.join(Dir.pwd, 'spec.en.ja.yml')
-      FileUtils.mkdir_p(Dir.pwd)
-      FileUtils.touch(@path)
-      @glossary = Glossary.new('spec', 'en', 'ja', Dir.pwd)
-    end
-
-    after do
-      FileUtils.remove_file(@path, true)
-    end
+    let(:glossary) { Glossary.new('spec', 'en', 'ja', Dir.pwd) }
+    let(:glossary_path) { File.join(Dir.pwd, 'spec.en.ja.yml') }
 
     describe '#add' do
-      context 'when source_term and target_term pair not exists' do
-        subject do
-          @glossary.add("spec", "スペック", "テストスペック")
-          yaml = YAML::load_file(@path)
-          yaml.index({"source_term"=>"spec", "target_term"=>"スペック", "note"=>"テストスペック"})
+      before do
+        FileUtils.remove_file(glossary_path, true)
+        glossary.create
+      end
+
+      context 'with arguments show new bilingual pair' do
+        before do
+          glossary.add("spec", "スペック", "テストスペック")
         end
-        it 'add source_term and target_term to specified glossary' do
-          should_not be_nil
+
+        it 'glossary yaml should have that bilingual pair' do
+          yaml = YAML::load_file(glossary_path)
+          term = yaml.index({"source_term"=>"spec", "target_term"=>"スペック", "note"=>"テストスペック"})
+          term.should_not be_nil
         end
       end
 
-      context 'when source_term and target_term pair already exists' do
+      context 'with arguments show existing bilingual pair' do
         before do
-          File.open(@path, "w") do |f|
-            f.puts([{"source_term" => "user", "target_term" => "ユーザ", "note" => "ユーザーではない"}].to_yaml)
-          end
+          glossary.add("user", "ユーザ", "ユーザーではない")
         end
 
-        subject {capture(:stdout){ @glossary.add("user", "ユーザ", "ユーザーではない") }}
-        it 'puts error message' do
+        subject{ capture(:stdout){ glossary.add("user", "ユーザ", "ユーザーではない") }}
+
+        it 'stdout should receive error message' do
           should eq "[user] [ユーザ] pair already exists\n"
         end
+      end
+
+      after do
+        FileUtils.remove_file(glossary_path, true)
       end
     end
   end
