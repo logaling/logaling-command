7cb4e4b13995009ecfe7b2003b7c7d9dc51d845f
SHIMADA Koji <koji.shimada@enishi-tech.com>
2011-12-09 23:59:22 +0900

    remove unnecessary process

--- lib/logaling/external_glossary/postgresql_manual.rb
+++ lib/logaling/external_glossary/postgresql_manual.rb
@@ -9,7 +9,7 @@ module Logaling
         doc = ::Nokogiri::HTML(open("http://osb.sraoss.co.jp/PostgreSQL/Manual/word.html", "r:iso-2022-jp").read.encode("utf-8"))
         doc.css('table table tr')[2..-1].each do |tr|
           if tr.children[2]
-            csv << ["#{tr.children[2].text}", "#{tr.children[4].text}"]
+            csv << [tr.children[2].text, tr.children[4].text]
           end
         end
       end
