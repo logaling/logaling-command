989dc82cdbc20b28038222d9fa81b74031b6af0c
SHIMADA Koji <koji.shimada@enishi-tech.com>
2012-01-21 12:11:23 +0900

    oops, fix copyright year

--- lib/logaling/external_glossaries/gene95.rb
+++ lib/logaling/external_glossaries/gene95.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2011 Koji SHIMADA
+# Copyright (C) 2012 Koji SHIMADA
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
