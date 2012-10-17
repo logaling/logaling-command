40a5c8a4aed233c16eebbdf4f25391746b35b236
SUZUKI Miho <adzuki34@gmail.com>
2012-10-17 18:27:08 +0900

    Remove comment

--- lib/logaling/glossary_db.rb
+++ lib/logaling/glossary_db.rb
@@ -64,7 +64,6 @@ module Logaling
     def deindex_glossary(glossary, glossary_source)
       delete_translations_by_glossary_source(glossary_source.source_path)
 
-      # TODO: wannna move to Glossary#enable_delete?
       glossary_source_num = 0;
       get_all_glossary_sources.each do |glossary_source_taken|
         if glossary_source.belongs_to_personal? && glossary_source_taken.belongs_to_personal?
